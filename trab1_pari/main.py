99247adbfddac0465688d83771871a7af798e657
Pedro Rolo <pmrolo@ua.pt>
2020-10-23 00:35:29 +0100

    conclusao da parte de defenir os argumentos, ficando a faltar por a cor em pari e fazer com que nao corra se nao forem dados argumentos

--- trab1_pari/main.py
+++ trab1_pari/main.py
@@ -4,4 +4,41 @@
 
 import  argparse
 
+testModo=argparse.ArgumentParser(description="Defenicao do modo de teste")
+# ao usar o action = ''store_true'' quando o comando e chamado ele returna o valor true quando nao e chama o valor false
+testModo.add_argument('-utm', '--use_time_mode', help='Max number of secs for time mode or maximum number of inputs for number of inputs mode', action="store_true")
+testModo.add_argument('-mv', '--max_value', help='Max number of secs for time mode or maximum number of inputs for number of inputs mode')
+
+tes=testModo.parse_args()
+
+
+
+''' ???ainda nao pos para nao correr sem os argumentos '''
+
+args_list = vars(tes)           # NB: importante converte o mamespace em dicionario atraves do vars
+
+
+# excuta para teste em modo tempo (por isso o True e verdadeiro pois foi chamado no argomento) senao executa para modo imput
+
+if args_list['use_time_mode']==True:
+    print(tes)
+    print ("PARI Typing Test, Grupo 2, Outober 2020") # ?????falta por pari em cor vermelho
+    print ("test running up to "+ str(args_list['max_value']) +" seconds.")
+    limiteTime=args_list['max_value']   # tempo maximo
+else:
+    print (tes)
+    print ("PARI Typing Test, Grupo 2, Outober 2020") #????? falta por a cor no pari
+    print ("test running up to " + str(args_list['max_value'])+" inputs")
+    limiteInputs=args_list['max_value']   # imputs maximo
+
+
+
+
+
+
+
+
+
+
+
 
